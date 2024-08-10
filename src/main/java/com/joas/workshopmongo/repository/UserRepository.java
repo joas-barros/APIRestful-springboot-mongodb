f8791405e3a17c930f94aba2fed454699dbaea3e
joas-barros <joas.barros14@hotmail.com>
2024-08-10 11:31:49 -0300

    connecting mongoDB with repository and service

---
+++ src/main/java/com/joas/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.joas.workshopmongo.repository;
+
+import com.joas.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
