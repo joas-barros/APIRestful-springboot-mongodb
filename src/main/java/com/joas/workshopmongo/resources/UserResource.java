11f37c44284c521af111d95efe09785d6484761e
joas-barros <joas.barros14@hotmail.com>
2024-08-11 11:27:00 -0300

    DTO pattern

--- src/main/java/com/joas/workshopmongo/resources/UserResource.java
+++ src/main/java/com/joas/workshopmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.joas.workshopmongo.resources;
 
 import com.joas.workshopmongo.domain.User;
+import com.joas.workshopmongo.dto.UserDTO;
 import com.joas.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -20,8 +21,9 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll(){
+    public ResponseEntity<List<UserDTO>> findAll(){
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDTO = list.stream().map(UserDTO::new).toList();
+        return ResponseEntity.ok().body(listDTO);
     }
 }
