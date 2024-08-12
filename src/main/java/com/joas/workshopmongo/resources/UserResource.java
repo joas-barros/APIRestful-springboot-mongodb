3dd5b4faa0d7ba01e7ac49ecc09339f8aed3b775
joas-barros <joas.barros14@hotmail.com>
2024-08-12 15:19:13 -0300

    delete User

--- src/main/java/com/joas/workshopmongo/resources/UserResource.java
+++ src/main/java/com/joas/workshopmongo/resources/UserResource.java
@@ -40,4 +40,10 @@ public class UserResource {
                 .buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
+
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
