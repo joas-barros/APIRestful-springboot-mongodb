1bd6f35b718df1bb59d74449043b16dc0337aaa8
joas-barros <joas.barros14@hotmail.com>
2024-08-12 07:36:29 -0300

    findbyId method and exceptions

--- src/main/java/com/joas/workshopmongo/services/UserService.java
+++ src/main/java/com/joas/workshopmongo/services/UserService.java
@@ -2,10 +2,12 @@ package com.joas.workshopmongo.services;
 
 import com.joas.workshopmongo.domain.User;
 import com.joas.workshopmongo.repository.UserRepository;
+import com.joas.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,5 +19,8 @@ public class UserService {
         return repo.findAll();
     }
 
-
+    public User findById(String id){
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
 }
---
+++ src/main/java/com/joas/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,7 @@
+package com.joas.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException{
+    public ObjectNotFoundException(String msg){
+        super(msg);
+    }
+}
