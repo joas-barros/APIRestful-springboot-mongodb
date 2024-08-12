d161e08d09368c4383b20898b0f59403e2e9f82e
joas-barros <joas.barros14@hotmail.com>
2024-08-12 08:53:28 -0300

    POST method

--- src/main/java/com/joas/workshopmongo/resources/UserResource.java
+++ src/main/java/com/joas/workshopmongo/resources/UserResource.java
@@ -5,13 +5,10 @@ import com.joas.workshopmongo.dto.UserDTO;
 import com.joas.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -32,5 +29,15 @@ public class UserResource {
     public ResponseEntity<UserDTO> findById(@PathVariable String id){
         UserDTO obj = new UserDTO(service.findById(id));
         return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody UserDTO objDTO){
+        User obj = service.fromDTO(objDTO);
+        obj = service.insert(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
     }
 }
--- src/main/java/com/joas/workshopmongo/services/UserService.java
+++ src/main/java/com/joas/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.joas.workshopmongo.services;
 
 import com.joas.workshopmongo.domain.User;
+import com.joas.workshopmongo.dto.UserDTO;
 import com.joas.workshopmongo.repository.UserRepository;
 import com.joas.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDTO){
+        return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
     }
 }
