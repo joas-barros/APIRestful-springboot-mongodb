aeee6349411af2def4759dd8cee7cac8cfd0dc8d
joas-barros <joas.barros14@hotmail.com>
2024-08-13 21:08:34 -0300

    creating entity post with agregated Users

---
+++ src/main/java/com/joas/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.joas.workshopmongo.repository;
+
+import com.joas.workshopmongo.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
