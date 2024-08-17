f64b816125574aee0af02242624cc6d1c2f3f8cd
joas-barros <joas.barros14@hotmail.com>
2024-08-17 09:25:51 -0300

    adding comments to post

--- src/main/java/com/joas/workshopmongo/domain/Post.java
+++ src/main/java/com/joas/workshopmongo/domain/Post.java
@@ -2,11 +2,14 @@ package com.joas.workshopmongo.domain;
 
 
 import com.joas.workshopmongo.dto.AuthorDTO;
+import com.joas.workshopmongo.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -20,6 +23,7 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() {
     }
@@ -62,6 +66,10 @@ public class Post implements Serializable {
 
     public void setBody(String body) {
         this.body = body;
+    }
+
+    public List<CommentDTO> getComments() {
+        return comments;
     }
 
     @Override
