fea2b1e2ac317303346a04b035b7cf4122d69b1e
joas-barros <joas.barros14@hotmail.com>
2024-08-19 14:18:44 -0300

    query methods

--- src/main/java/com/joas/workshopmongo/resources/PostResource.java
+++ src/main/java/com/joas/workshopmongo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.joas.workshopmongo.resources;
 import com.joas.workshopmongo.domain.Post;
 import com.joas.workshopmongo.domain.User;
 import com.joas.workshopmongo.dto.UserDTO;
+import com.joas.workshopmongo.resources.util.URL;
 import com.joas.workshopmongo.services.PostService;
 import com.joas.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +25,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = "/titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
